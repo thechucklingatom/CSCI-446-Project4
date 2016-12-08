86a9f9761ebf09add664201f971b21d066123ee2
Robert Putnam <thechucklingatom@gmail.com>
2016-12-08 09:10:35 -0700

    world generation done, just need to test now.

--- src/CSCI446/Project4/Main.java
+++ src/CSCI446/Project4/Main.java
@@ -1,10 +1,16 @@
 package CSCI446.Project4;
 
+import java.io.File;
+
 /**
  * Created by thechucklingatom on 12/6/2016.
  */
 public class Main {
 	public static void main(String[] args){
+		WorldGenerator worldGenerator = new WorldGenerator("tracks" +
+				File.separator +
+		"L-track.txt");
 
+		worldGenerator.generateWorld();
 	}
 }
--- src/CSCI446/Project4/WorldGenerator.java
+++ src/CSCI446/Project4/WorldGenerator.java
@@ -1,7 +1,9 @@
 package CSCI446.Project4;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 /**
  * Created by thechucklingatom on 12/7/16.
@@ -20,6 +22,39 @@ public class WorldGenerator {
 	}
 
 	public void generateWorld(){
+		BufferedReader reader = new BufferedReader(fileReader);
+		try {
+			String line1 = reader.readLine();
+			String[] sizes = line1.split(",");
+			worldHeight = Integer.valueOf(sizes[0]);
+			worldWidth = Integer.valueOf(sizes[1]);
+			Tile[][] world = new Tile[worldHeight][worldWidth];
 
+			String currentLine;
+			int rowCounter = 0;
+
+			while((currentLine = reader.readLine()) != null){
+				char[] charArray = currentLine.toCharArray();
+				for (int i = 0; i < charArray.length; i++) {
+					Tile toAdd;
+					if(charArray[i] == '#') {
+						toAdd = new Tile(Tile.TileType.WALL);
+					}else if(charArray[i] == 'S'){
+						toAdd = new Tile(Tile.TileType.START);
+					}else if(charArray[i] == 'F'){
+						toAdd = new Tile(Tile.TileType.FINISH);
+					}else{
+						toAdd = new Tile(Tile.TileType.SAFE);
+					}
+
+					world[rowCounter][i] = toAdd;
+				}
+				rowCounter++;
+			}
+
+			generatedWorld = new World(world);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
