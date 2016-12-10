0fb62f82e264127d40886b89f23626cbc3cfc102
Robert Putnam <thechucklingatom@gmail.com>
2016-12-10 11:50:56 -0700

    more skeleton stuff

--- src/CSCI446/Project4/Main.java
+++ src/CSCI446/Project4/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	public static void main(String[] args){
 		WorldGenerator worldGenerator = new WorldGenerator("tracks" +
 				File.separator +
-		"L-track.txt");
+				"L-track.txt");
 
 		worldGenerator.generateWorld();
 	}
---
+++ src/CSCI446/Project4/ValueIteration.java
@@ -0,0 +1,7 @@
+package CSCI446.Project4;
+
+/**
+ * Created by thechucklingatom on 12/10/2016.
+ */
+public class ValueIteration {
+}
