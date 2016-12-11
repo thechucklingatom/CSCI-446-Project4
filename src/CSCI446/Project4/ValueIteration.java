d058984a7cdabdbe47f98d41f71d5d6723547d11
fraticellics <fraticellics@gmail.com>
2016-12-10 18:13:03 -0700

    committing to pull

--- src/CSCI446/Project4/ValueIteration.java
+++ src/CSCI446/Project4/ValueIteration.java
@@ -1,5 +1,6 @@
 package CSCI446.Project4;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,48 +17,59 @@ public class ValueIteration {
 			delta , the maximum change in the utility of any state in an iteration
 
 	repeat
-		U←U’ ; ←0
+		U←U’ ; delta←0
 		for each state s in S do
-			U’[s]←R(s) + argmax(P(s’ | s,a)U[s’])
-			if U’[s] - U[s] >  then  ←U’[s] - U[s]
-	until delta < e(1 - y)y /
+			U’[s]←R(s) + y argmax(P(s’ | s,a)U[s’])
+			if U’[s] - U[s] > delta then delta ←U’[s] - U[s]
+	until delta < e(1 - y)/y
 	return U
 
 	*/
 
 	private double epsilon;
 	private double maxChange = .5;
-	private double discount = .9;
+	private final double discount = .9;
+	private double reward = -.04;
 	private World world;
+	private List<State> states;
 
 	public ValueIteration(World world) {
 		this.world = world;
+		states = new ArrayList<State>();
 	}
 
-	public void calculateUtilities(){
+	public StateAction calculateUtilities(){
+		//this is the main iterator that will terminate when the largest change of
+		//utility is below a threshold determined by the discount and epsilon
 		while(!(maxChange < epsilon * (1 - discount) / discount)){
-			maxChange = .5;
+			maxChange = 0;
+			for(State s : states){
+				double newUtility = reward + Math.max(;
+				)
+			}
 
-			for(Tile[] tiles : world.theWorld){
-				for(Tile tile : tiles){
-					if(tile.type == Tile.TileType.WALL){
-						continue;
-					}
-					List<Action> actions = tile.actions;
-					double currentReward = tile.getReward();
-					double currentUtility = tile.getCurrentUtility();
-					if(currentUtility == 0){
-						currentUtility = tile.getReward();
-					}
-
-					for(Action action : actions){
-
-					}
-
-				}//inner for
-			}//outer for
 		}//while
+		return null;
 	}
 
+	public void generateS(){
+		for(Tile[] tiles : world.theWorld){
+			for(Tile tile : tiles){
+				if(tile.type == Tile.TileType.WALL){
+					continue;
+				}
+				List<Action> actions = tile.actions;
+				double currentReward = tile.getReward();
+				double currentUtility = tile.getCurrentUtility();
+				if(currentUtility == 0){
+					currentUtility = tile.getReward();
+				}
 
+				for(Action action : actions){
+
+				}
+
+			}//inner for
+		}//outer for
+	}
 }
