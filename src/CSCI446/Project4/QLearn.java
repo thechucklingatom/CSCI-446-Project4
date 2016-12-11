6c0f836929ebc5edab51326c3a9f644f13602fa5
MathewGostnell <mathew.gostnell@gmail.com>
2016-12-10 17:15:31 -0700

    Tweaking QLearn v2.0

--- src/CSCI446/Project4/QLearn.java
+++ src/CSCI446/Project4/QLearn.java
@@ -10,66 +10,49 @@ import java.util.Set;
  */
 public class QLearn {
 
-    private Map<StateAction, Integer> n;    // table
-    private Action a = null;
-    private State p = null;
+    private Map<StateAction, Integer> n;    // table mapping StateAction frequencies
+    private Action a = null;    // last action taken
+    private State p = null;     // the previous state visited
 
     private double r = 0.f;
     private double a = 1.f;
 
-    public Action QLearnAgent(State e) {
-        State j = e;
+    public Action QLearnAgent(State e, Action j) {
         if (p != null) {
-            if (n.containsKey(new StateAction(e, a))) { // update the existing state-action frequency
-                n.put(new StateAction(e, a), n.get(new StateAction(e, a)) + 1);
+            // N[p, a] <- N[p, a] + 1
+            StateAction sa = new StateAction(p, a);
+            if (n.containsKey(new StateAction(p, a))) { // update the existing state-action frequency
+                n.put(new StateAction(p, a), n.get(new StateAction(p, a)) + 1);
             } else { // insert new frequency count into our table
                 n.put(new StateAction(e, a), 1);
             }
+
             // Q[i, a] = Q[i, a] + a(r + max of
         }
+        if (e.getTile().type.equals(Tile.TileType.FINISH)) {
+            p = null;
+        } else {
+
+        }
+        return null;
     }
-
-    /*
-    Q(s,a) ← Q(s, a) + (r + maxa’ Q(s’,a’) - Q(s,a))
-Where the learning rate, , is ∈[0,1] and is a discount assigned value that is associated with weighting updates to our Q(s,a) table.
-
-
+/*
 function Q-LEARNiNG-AGENT(e) returns an action
     static: 	Q, a table of action values
     		    N, a table of state-action frequencies
                 a, the last action taken
-                i, the previous state visited
-                r, the reward received in state i
+                p, the previous state visited
+                r, the reward received in state p
     j ← STATE[e]
-    if i is non-null then
-        N[a,i] ← N[a,i] + 1
-        Q[a,i] ← Q[a,i] + α(r + maxa’ Q[a’,j] - Q[a,i])
+    if p is non-null then
+        N[a,p] ← N[a,p] + 1
+        Q[a,p] ← Q[a,p] + α(r + maxa’ Q[a’,j] - Q[a,p])
     if TERMINAL?[e] then
-        i ← null
+        p ← null
     else
-        i ← j
+        p ← j
         r ← REWARD[e]
     a ← arg maxa’, f(Q[a',j], N[a',j])
  return a
-
      */
-
-}
-
-class StateAction {
-    // used as entry into the table of state-action frequencies
-    private final State s;
-    private final Action a;
-
-    public StateAction(State s, Action a) {
-        this.s = s;
-        this.a = a;
-    }
-
-    public State getState() { return s; }
-    public Action getAction() { return a; }
-    public void printStateAction() {
-        System.out.println(s.toString());
-        a.printAction();
-    }
 }
---
+++ src/CSCI446/Project4/StateAction.java
@@ -0,0 +1,22 @@
+package CSCI446.Project4;
+
+/**
+ * Created by Thew on 12/10/2016.
+ */
+public class StateAction {
+    // used as entry into the table of state-action frequencies
+    private final State s;
+    private final Action a;
+
+    public StateAction(State s, Action a) {
+        this.s = s;
+        this.a = a;
+    }
+
+    public State getState() { return s; }
+    public Action getAction() { return a; }
+    public void printStateAction() {
+        System.out.println(s.toString());
+        a.printAction();
+    }
+}
