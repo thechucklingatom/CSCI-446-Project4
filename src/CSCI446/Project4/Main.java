ecda472c3a4106ea2ddd1aef8ba1e49ef849fac0
Robert Putnam <thechucklingatom@gmail.com>
2016-12-11 20:46:18 -0700

    push for main

--- src/CSCI446/Project4/Main.java
+++ src/CSCI446/Project4/Main.java
@@ -1,6 +1,10 @@
 package CSCI446.Project4;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.DoubleSummaryStatistics;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 /**
  * Created by thechucklingatom on 12/6/2016.
@@ -13,6 +17,11 @@ public class Main {
 
 		worldGenerator.generateWorld();
 		System.out.println(worldGenerator);
+
+		ValueIteration valueIteration = new ValueIteration(worldGenerator.generatedWorld);
+		valueIteration.runValueIteration();
+
+		System.out.println("Iteration done");
 
 		QLearn qLearn = QLearn.getInstance(worldGenerator.generatedWorld,
 				new State(worldGenerator.generatedWorld.startTile, new Velocity()));
